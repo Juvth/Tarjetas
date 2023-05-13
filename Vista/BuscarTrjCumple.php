e31d544e74111816c1fd6c9835b6f350eda06bc2
Anderson Santana <98790211+Juvth@users.noreply.github.com>
2023-05-13 16:37:31 -0500

    Correcciones

--- Vista/BuscarTrjCumple.php
+++ Vista/BuscarTrjCumple.php
@@ -8,7 +8,8 @@
     <link rel="stylesheet" href="../CSS/styles.css">
     <link rel="stylesheet" href="../CSS/admin.css">
     <link rel="stylesheet" href="../CSS/buscar.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" 
+    integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
     <script src="../Modelo/AjaxTB.js"></script>
 </head>
 <body style="background-image:none;">
