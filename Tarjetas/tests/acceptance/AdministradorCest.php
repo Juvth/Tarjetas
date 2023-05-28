950cb0575113e306a453c705c573ebaf3837413d
Anderson Santana <98790211+Juvth@users.noreply.github.com>
2023-05-28 09:38:29 -0500

    Test

--- Tarjetas/tests/acceptance/AdministradorCest.php
+++ Tarjetas/tests/acceptance/AdministradorCest.php
@@ -6,29 +6,8 @@ class AdministradorCest
     {
     }
 
-     // tests
-     public function registroAdmin(AcceptanceTester $I)
-     {
-         // Ingresar a la página de registro admin
-         $I->amOnPage('/ruta-del-formulario-de-registro-admin');
- 
-         // Rellenar el formulario con datos de prueba
-        
-         $I->fillField('nombre', 'DennysM');
-         $I->fillField('contraseña', '12345');
-        
- 
-         // Enviar el formulario
-         $I->click('Enviar');
- 
-         // Verificar que se redireccionó correctamente después del registro
-         $I->seeInCurrentUrl('/Admin.html');
- 
-         // Verificar que se haya insertado el grado en la base de datos
-         $I->seeInDatabase('administrador', [
-             
-             'nombre' => 'DennysM',
-             'contraseña' => '12345',
-         ]);
-     }
+    // tests
+    public function tryToTest(AcceptanceTester $I)
+    {
+    }
 }
--- Tarjetas/tests/acceptance/RegistroClienteCest.php
+++ Tarjetas/tests/acceptance/RegistroClienteCest.php
@@ -4,36 +4,10 @@ class RegistroClienteCest
 {
     public function _before(AcceptanceTester $I)
     {
-        // Configuración inicial, si es necesaria
     }
 
     // tests
-    public function registroCliente(AcceptanceTester $I)
+    public function tryToTest(AcceptanceTester $I)
     {
-        // Ingresar a la página de registro clientes
-        $I->amOnPage('/ruta-del-formulario-de-registro-cliente');
-
-        // Rellenar el formulario con datos de prueba
-        $I->fillField('direccion', 'Avenida simon bolivar y bolivar bonilla');
-        $I->fillField('usuario', 'DennysMejia');
-        $I->fillField('telefono', '0983476533');
-        $I->fillField('cedula', '0605830025');
-        $I->fillField('contraseña', '12345');
-       
-
-        // Enviar el formulario
-        $I->click('Enviar');
-
-        // Verificar que se redireccionó correctamente después del registro
-        $I->seeInCurrentUrl('/index.html');
-
-        // Verificar que se haya insertado el grado en la base de datos
-        $I->seeInDatabase('cliente', [
-            'direccion' => 'Avenida simon bolivar y bolivar bonilla',
-            'usuario' => 'DennysMejia',
-            'telefono' => '0983476533',
-            'cedula' => '0605830025',
-            'contraseña' => '12345',
-        ]);
     }
 }
