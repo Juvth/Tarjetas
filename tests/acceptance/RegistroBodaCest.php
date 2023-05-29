a5dd6ab53bf481f0ac5ca60849a29d19abc9eb35
PatoV16 <133371436+PatoV16@users.noreply.github.com>
2023-05-29 09:39:20 -0500

    Test

--- tests/acceptance/RegistroBodaCest.php
+++ tests/acceptance/RegistroBodaCest.php
@@ -1,13 +1,44 @@
 <?php
+// tests/acceptance/RegistroBodaCest.php
 
 class RegistroBodaCest
 {
     public function _before(AcceptanceTester $I)
     {
+        // Configuración inicial, si es necesaria
     }
 
-    // tests
-    public function tryToTest(AcceptanceTester $I)
+    // Prueba de aceptación para el registro de una boda
+    public function registroBoda(AcceptanceTester $I)
     {
+        // Ingresar a la página de registro de boda
+        $I->amOnPage('/ruta-del-formulario-de-registro-boda');
+
+        // Rellenar el formulario con datos de prueba
+        $I->fillField('nombre_Novio', 'Nombre del Novio');
+        $I->fillField('nombre_Novia', 'Nombre de la Novia');
+        $I->fillField('id_boda', '123');
+        $I->fillField('fechaBoda', '2023-01-01');
+        $I->fillField('horaBoda', '10:00');
+        $I->fillField('padrinosBoda', 'Padrinos de la Boda');
+        $I->fillField('cedula', '1234567890');
+
+        // Enviar el formulario
+        $I->click('Enviar');
+
+        // Verificar que se redireccionó correctamente después del registro
+        $I->seeInCurrentUrl('/TipoDeTarjeta.html');
+
+        // Verificar que se haya insertado la boda en la base de datos
+        $I->seeInDatabase('trj_boda', [
+            'nombre_Novio' => 'Nombre del Novio',
+            'nombre_Novia' => 'Nombre de la Novia',
+            'id_boda' => '123',
+            'fecha' => '2023-01-01',
+            'hora' => '10:00',
+            'padrinos' => 'Padrinos de la Boda',
+            'cedula_cliente' => '1234567890',
+        ]);
     }
 }
+
